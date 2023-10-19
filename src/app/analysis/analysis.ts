ae74ca314d0cbbd9de7b4e8ff81f25d97e59acde
dededec <daviderenacasado@gmail.com>
2023-10-19 09:18:37 +0200

    Complete analysis class

--- src/app/analysis/analysis.ts
+++ src/app/analysis/analysis.ts
@@ -1,4 +1,10 @@
 export class Analysis {
     url:string;
+    title:string;
+    description:string;
+    keywords:string[];
+    headings:Object;
+    html5:boolean;
+    images:number;
     lastModified:Date
 }
